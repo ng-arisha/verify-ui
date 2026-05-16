e3c6786f0062d5d6ceb13b814d46c5bfcf5d9072
Brian-mula <mulatiwangilabrian@gmail.com>
2026-05-16 14:28:39 +0300

    Chores escape

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         {
           method: "GET",
           headers: {
-            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true",
           },
           // body: JSON.stringify({ receiptNo: searchValue }),
         }
